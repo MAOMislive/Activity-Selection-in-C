4ceea332e1bcaf957c2ab4667db46c3a4d980d2a
Mohammad Abu Obaida Mullick <86158154+MAOMislive@users.noreply.github.com>
2023-02-25 03:00:17 +0600

    Update FractionalKnapsack.c

--- FractionalKnapsack.c
+++ FractionalKnapsack.c
@@ -3,61 +3,53 @@
 
 #define MAX_ITEMS 100
 
-int main()
-{
+// define a struct to represent an item
+typedef struct {
+    float value;
+    float weight;
+    float ratio;
+} Item;
+
+int main() {
     int n, W;
-    float items[MAX_ITEMS][3];
+    Item items[MAX_ITEMS]; // create an array of Item structs
     printf("Enter the number of items: ");
     scanf("%d", &n);
     printf("Enter the capacity of the knapsack: ");
     scanf("%d", &W);
     printf("Enter the value and weight of each item:\n");
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%f %f", &items[i][0], &items[i][1]);
-        items[i][2] = items[i][0] / items[i][1];  // store ratio
+    // loop through the items and store their value, weight, and ratio
+    for (int i = 0; i < n; i++) {
+        scanf("%f %f", &items[i].value, &items[i].weight);
+        items[i].ratio = items[i].value / items[i].weight; // calculate the ratio
     }
-    // sort items in descending order of their ratio
-    for (int i = 0; i < n-1; i++)
-    {
-        for (int j = i+1; j < n; j++)
-        {
-            if (items[i][2] < items[j][2])
-            {
-                float temp_ratio = items[i][2];
-                float temp_value = items[i][0];
-                float temp_weight = items[i][1];
-                items[i][2] = items[j][2];
-                items[i][0] = items[j][0];
-                items[i][1] = items[j][1];
-                items[j][2] = temp_ratio;
-                items[j][0] = temp_value;
-                items[j][1] = temp_weight;
+    // sort the items in descending order of their ratio
+    Item temp;
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (items[j].ratio > items[i].ratio) {
+                // swap the items
+                temp = items[i];
+                items[i] = items[j];
+                items[j] = temp;
             }
         }
     }
-    float P = 0.0;
-    // loop through items and add to knapsack
-    for (int i = 0; i < n; i++)
-    {
-        if (W >= items[i][1])
-        {
-            // add entire item to knapsack
-            P += items[i][0];
-            W -= items[i][1];
-        }
-        else
-        {
-            // add a fraction of the item to knapsack
-            float fraction = (float) W / items[i][1];
-            P += fraction * items[i][0];
+    // fill the knapsack with items until it's full
+    float totalValue = 0;
+    int i = 0;
+    while (W > 0 && i < n) {
+        if (items[i].weight <= W) {
+            // take the whole item
+            totalValue += items[i].value;
+            W -= items[i].weight;
+        } else {
+            // take a fraction of the item
+            totalValue += W * items[i].ratio;
             W = 0;
         }
-        if (W == 0)
-        {
-            break;
-        }
+        i++;
     }
-    printf("Total value of items in the knapsack: %f\n", P);
+    printf("Total value of the items in the knapsack is: %.2f\n", totalValue);
     return 0;
 }
